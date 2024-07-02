792caa3a1a77cd7fdbcd1cd9b1d327e81d8025c7
Yogee-s <yogeeswaran99@gmail.com>
2024-07-02 14:02:32 +0800

    fixed untracked files

--- dapg/dapg/demonstrations/download.sh
+++
@@ -1,5 +0,0 @@
-gdown --id 1TY_zZ_rJGUFPhPH2jI6DPilvwUIjRcHV 
-gdown --id 1VcEzQI_L6CvwILJLaBQacN69ZCaFFnV1 
-gdown --id 1mvxIouKbb_UleNJn0jtWkQZecUmrcwip 
-gdown --id 1Upl6i2QMHcwEwlB2-P0NNqqBdZXpKn2J 
-gdown --id 1ndy7rNqzEpp750Esn-bfyBtmeOr7kNau
